9decfc378558b51e3206ef58baabee892040b8a4
Jeff Loyd <jeffloyd@gmail.com>
2024-05-28 17:11:42 -0500

    adding compiling support

---
+++ out/frontend/src/components/About.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../css/About.css';
+
+const About: React.FC = () => {
+
+  return (
+    <div className="dashboard about-dashboard">
+      <h2>About</h2>
+      <div className="about-container">
+        Sandy in a box!
+        <br/>
+      </div>
+    </div>
+  );
+};
+
+export default About;
